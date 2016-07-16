e2210e52ffb11e827c8de5f97ead56209deeb200
AlexKrasnov <ya.sasha-krasnov@yandex.ru>
2016-07-16 23:54:27 +0300

    class Hyperbola

--- Constraints/Constraints/Constraints.cpp
+++ Constraints/Constraints/Constraints.cpp
@@ -33,10 +33,10 @@ Ellipse::Ellipse(double a1, double b1, double x1, double y1)
 	y0 = y1;
 }
 
-void Ellipse::Output()
+double Ellipse::Calculate(Point pt)
 {
-	cout << "(x-" << x0 << ")^2/" << a * a << "+ (y - "
-		<< y0 << ")^2/" << b * b;
+	return (pt.x - x0) * (pt.x - x0)/(a * a)+
+		(pt.y - y0)*(pt.y - y0) / (b * b);
 }
 
 void Ellipse::Input()
@@ -44,11 +44,37 @@ void Ellipse::Input()
 	cin >> a >> b >> x0 >> y0;
 }
 
-double Ellipse::Calculate(Point pt)
+void Ellipse::Output()
 {
-	return (pt.x - x0) * (pt.x - x0)/(a * a)+
-		(pt.y - y0)*(pt.y - y0) / (b * b);
+	cout << "(x-" << x0 << ")^2/" << a * a << "+ (y - "
+		<< y0 << ")^2/" << b * b;
 }
 
 //-------------------------------------------------------------
 
+Hyperbola::Hyperbola(double a1, double b1, double x1, double y1)
+{
+	if(a1 == 0 || b1 == 0)
+		throw "если параметр a или b равен нулю, эллиптическую функцию определить невозможно";
+	a = a1;
+	b = b1;
+	x0 = x1;
+	y0 = y1;
+}
+
+double Hyperbola::Calculate(Point pt)
+{
+	return (pt.x - x0) * (pt.x - x0)/(a * a) -
+		(pt.y - y0)*(pt.y - y0) / (b * b);
+}
+
+void Hyperbola::Input()
+{
+	cin >> a >> b >> x0 >> y0;
+}
+
+void Hyperbola::Output()
+{
+	cout << "(x-" << x0 << ")^2/" << a * a << "- (y - "
+		<< y0 << ")^2/" << b * b;
+}
